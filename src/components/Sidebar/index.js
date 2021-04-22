27142d5ac27b83275c3c4c27f96ab04ce980b494
Gustavo Xavier <gustasv00@gmail.com>
2021-04-22 14:20:09 -0300

    removendo teste e inidicando template

--- src/components/Sidebar/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Sidebar = () => <h1>Sidebar</h1>;
-
-export default Sidebar;
--- src/components/Sidebar/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Sidebar = styled.aside`
-  background: ${(props) => props.theme.colors.background};
-  min-width: 20rem;
-  max-height: 100hv;
-  overflow-y: hidden;
-`;
