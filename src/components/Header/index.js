57fd98c623b705dbf99d502fe2aace52e5c4b0c3
gustxavier <gustasv00@gmail.com>
2021-06-21 19:03:03 -0300

    dev.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
