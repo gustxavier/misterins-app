c76e2ac42b9c1ed9cde95b20e550440323271186
gustxavier <gustasv00@gmail.com>
2021-07-08 18:43:45 -0300

    atualizar senha

--- src/pages/Password/Recouver/index.js
+++ src/pages/Password/Recouver/index.js
@@ -1,11 +1,19 @@
 import React from "react";
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 import api from "../../../services/api";
 
 import "./styles.css";
-import { Button, Container, Grid, Typography } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  Grid,
+  Typography,
+  CircularProgress,
+} from "@material-ui/core";
 import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
+import HighlightOffOutlinedIcon from "@material-ui/icons/HighlightOffOutlined";
+import { SimpleNoty } from "../../../helpers/NotyFeedBack";
 
 class Recouver extends React.Component {
   constructor(props) {
@@ -16,10 +24,13 @@ class Recouver extends React.Component {
       hash: props.match.params.hash,
       password: "",
       password_confirm: "",
+      return: false,
+      icon: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCheckLinkExpired = this.handleCheckLinkExpired.bind(this);
   }
 
   handleChange(event) {
@@ -30,26 +41,59 @@ class Recouver extends React.Component {
 
   async handleSubmit(event) {
     event.preventDefault();
-    try {
-      this.setState({ loading: true });
-      
-    } catch (err) {
-      this.setState({ loading: false });
-    }
-  }
-
-  handleClick(event) {
-    this.props.history.push("/register");
+    this.setState({ spinner: true });
+    let self = this
+    api
+      .post("recouver-password", this.state)
+      .then((res) => {
+        if (res.data.status) {
+          SimpleNoty(
+            "Senha atualizada com sucesso. Você já pode acessar a plataforma com sua nova senha!",
+            "success"
+          );
+          self.props.history.push("/");
+        } else {
+          SimpleNoty(
+            "Oops! Falha ao renovar sua senha. Entre em contato com o adminsitrador do sistema",
+            "danger"
+          );
+          self.props.history.push("/");
+        }
+      })
+      .catch(function (error) {
+        self.setState({ spinner: false });
+        if (error) {
+          console.log(error);
+        }
+      });
   }
 
   componentDidMount() {
-    console.log(this.state.hash)
+    this.handleCheckLinkExpired();
+
     ValidatorForm.addValidationRule("isPasswordMatch", (value) => {
       if (value !== this.state.password) {
         return false;
       }
       return true;
     });
+  }
+
+  handleCheckLinkExpired() {
+    api
+      .get("check-link-recouver-password/" + this.state.hash)
+      .then((res) => {
+        if (res.data.status) {
+          this.setState({
+            spinner: false,
+            return: res.data.msg,
+            icon: res.data.icon,
+          });
+        }
+      })
+      .catch((res) => {
+        console.log("err. " + res);
+      });
   }
 
   componentWillUnmount() {
@@ -60,56 +104,93 @@ class Recouver extends React.Component {
 
   render() {
     return (
-      <Container maxWidth="xl">
-        <div className="logon-container">
-          <section className="form card">
-            <div className="row m-0 mb-2">
-              <div className="col-md-4 pb-1 bg-dark"></div>
-              <div className="col-md-4 pb-1 bg-primary"></div>
-              <div className="col-md-4 pb-1 bg-danger"></div>
-            </div>            
-            <ValidatorForm
-              ref={(r) => (this.form = r)}
-              onSubmit={this.handleSubmit}
-            >
+      <Container maxWidth="lg">
+        <div className="recouver-container">
+          {this.state.spinner && (
+            <div id="spinner-live" className="spinner">
+              <CircularProgress />
+            </div>
+          )}
+          {!this.state.return ? (
+            <section className="form card">
+              <div className="row m-0 mb-2">
+                <div className="col-md-4 pb-1 bg-dark"></div>
+                <div className="col-md-4 pb-1 bg-primary"></div>
+                <div className="col-md-4 pb-1 bg-danger"></div>
+              </div>
+              <ValidatorForm
+                ref={(r) => (this.form = r)}
+                onSubmit={this.handleSubmit}
+              >
+                <Grid container>
+                  <Grid item sm={12}>
+                    <Typography
+                      className="text-white text-center mb-4 mt-0"
+                      variant="h6"
+                    >
+                      Nova senha
+                    </Typography>
+                    <TextValidator
+                      label="Digite uma senha"
+                      variant="outlined"
+                      type="password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.handleChange}
+                      validators={["required", "matchRegexp:^.{8,}$"]}
+                      errorMessages={[
+                        "Por favor, insira uma senha",
+                        "Mínimo 8 caracteres",
+                      ]}
+                    />
+                    <TextValidator
+                      label="Confirme sua senha"
+                      variant="outlined"
+                      type="password"
+                      name="password_confirm"
+                      value={this.state.password_confirm}
+                      onChange={this.handleChange}
+                      validators={["isPasswordMatch", "required"]}
+                      errorMessages={[
+                        "As senhas não parecem ser iguais",
+                        "Por favor, confirme sua senha",
+                      ]}
+                    />
+                    <Button className="button" type="submit">
+                      <span className="inline">
+                        <Typography>Salvar</Typography>
+                      </span>
+                    </Button>
+                  </Grid>
+                </Grid>
+              </ValidatorForm>
+            </section>
+          ) : (
+            <div>
               <Grid container>
                 <Grid item sm={12}>
-                  <Typography className="text-white text-center mb-4 mt-0" variant="h6">Nova senha</Typography>
-                  <TextValidator
-                        label="Digite uma senha"
-                        variant="outlined"
-                        type="password"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                        validators={["required", "matchRegexp:^.{8,}$"]}
-                        errorMessages={[
-                          "Por favor, insira uma senha",
-                          "Mínimo 8 caracteres",
-                        ]}
+                  <img
+                    src="https://misterins.com.br/wp-content/themes/misterins/assets/images/logo.png"
+                    alt="Mister Ins"
+                  />
+                  <Typography
+                    className="text-white text-center mb-4 mt-5"
+                    variant="h5"
+                  >
+                    {this.state.return}
+                  </Typography>
+                  <div>
+                    <div className="text-center">
+                      <HighlightOffOutlinedIcon
+                        fontSize="large"
+                        className="icon-danger mt-4"
                       />
-                      <TextValidator
-                        label="Confirme sua senha"
-                        variant="outlined"
-                        type="password"
-                        name="password_confirm"
-                        value={this.state.password_confirm}
-                        onChange={this.handleChange}
-                        validators={["isPasswordMatch", "required"]}
-                        errorMessages={[
-                          "As senhas não parecem ser iguais",
-                          "Por favor, confirme sua senha",
-                        ]}
-                      />
-                  <Button className="button" type="submit">
-                    <span className="inline">
-                      <Typography>Salvar</Typography>
-                    </span>
-                  </Button>
+                    </div>
+                  </div>
                 </Grid>
               </Grid>
-            </ValidatorForm>
-          </section>
+            </div>
+          )}
         </div>
       </Container>
     );
